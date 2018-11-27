57ddd0bd275b49126cf5e20d9b7525c300b02b25
Jan Trejbal <jan.trejbal@gmail.com>
2018-11-27 02:17:55 +0100

    Drop vulnerable packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 ﻿const gulp = require('gulp');
 const ts = require('gulp-typescript');
-const eventStream = require('event-stream');
-const tsProject = ts.createProject('tsconfig.json');
 const tslint = require('gulp-tslint');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
@@ -30,27 +28,38 @@ gulp.task('lint:ts', () =>
         .pipe(tslint.report())
 );
 
-gulp.task('build:ts', ['lint:ts'], () => {
-    const tsResult = tsProject.src()
-        .pipe(sourcemaps.init())
-        .pipe(tsProject().on('error', function () { process.exit(1) }));
+const tsResult = () => {
+	const tsProject = ts.createProject('tsconfig.json');
 
-    return eventStream.merge(
-        tsResult.dts
-            .pipe(concat(config.dtsBundle))
-            .pipe(gulp.dest(config.dist)),
-        tsResult.js
-            .pipe(concat(config.jsBundle))
-            .pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest(config.dist))
-    );
-});
+	return tsProject.src()
+		.pipe(sourcemaps.init())
+		.pipe(tsProject().on('error', function () {
+			process.exit(1)
+		}))
+};
 
-gulp.task('minify:js', ['build:ts'], () =>
-    gulp.src(config.dist + '/' + config.jsBundle)
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(config.dist))
+gulp.task('build:ts:dts',
+	() => tsResult().dts
+		.pipe(concat(config.dtsBundle))
+		.pipe(gulp.dest(config.dist))
 );
 
-gulp.task('default', ['minify:js', 'build:css']);
+gulp.task('build:ts:js',
+	() => tsResult().js
+		.pipe(concat(config.jsBundle))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(config.dist))
+);
+
+gulp.task('minify:js', () =>
+	gulp.src(config.dist + '/' + config.jsBundle)
+		.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest(config.dist))
+);
+
+gulp.task('build:ts', gulp.parallel('lint:ts', 'build:ts:dts', 'build:ts:js'));
+
+gulp.task('build:js', gulp.series('build:ts','minify:js'));
+
+gulp.task('default', gulp.parallel('build:js', 'build:css'));
